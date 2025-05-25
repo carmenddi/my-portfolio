f18cb35cb669fb5d40483f59637f845ee9a36b8c
carmenddi <carmendarca@gmail.com>
2025-05-25 09:52:20 +0200

    update

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,11 +1,12 @@
 import './ProjectCard.css';
 
-export default function ProjectCard({ image, title, link }) {
+export default function ProjectCard({ image, title, link, description }) {
     return (
         <div className="project-card">
           <img src={image} alt={title} className="project-image" />
           <div className="elements">
-            <p className="project-title">{title}</p>
+            <h3 className="project-title">{title}</h3>
+            <p className="project-description">{description}</p>
             <button className="view-more-btn"><a href={link} target="_blank" rel="noreferrer">Visit Website</a></button>
           </div>
         </div>
--- src/Projects.js
+++ src/Projects.js
@@ -10,10 +10,10 @@ export default function Projects () {
   const projectsRef = useRef(null);
   const isVisible = useInView(projectsRef);
   const projectData = [
-    { image: beyondWatches, title: "Beyond Watches", link: 'https://beyondwatchesbw.com' },
-    { image: littleLemon, title: "Little Lemon", link:'https://projectlittlelemon.netlify.app/' },
-    { image: halfmoon, title: "Halfmoon", link: 'https://carmenddi.github.io/halfmoon/' },
-    { image: hairDay, title: "Hair Day", link: 'https://projecthairday.netlify.app' }
+    { image: beyondWatches, title: "Beyond Watches", description: "A WordPress blog for a watch enthusiast, designed to showcase articles and reviews in a clean, elegant layout. Focused on readability, responsive design, and easy content management.  ", link: 'https://beyondwatchesbw.com' },
+    { image: littleLemon, title: "Little Lemon", description: "A modern, responsive website for a restaurant, built with React. Focus on performance, component-based structure, and a smooth user experience across devices.", link:'https://projectlittlelemon.netlify.app/' },
+    { image: halfmoon, title: "Halfmoon", description: "A Bootstrap-based restaurant website featuring a sleek, mobile-first layout. Designed for quick navigation, visual appeal, and clear presentation of key information.", link: 'https://carmenddi.github.io/halfmoon/' },
+    { image: hairDay, title: "Hair Day", description: "Modern website for a hair salon, built with React. Features a photo gallery showcasing the work, service information, and an appointment booking form to enhance user experience.", link: 'https://projecthairday.netlify.app' }
   ];
     return (
         <>
@@ -22,7 +22,7 @@ export default function Projects () {
               <h2 id="projects-section">Projects</h2>
               <div className="projects-container">
                {projectData.map((project, index) => (
-               <ProjectCard key={index} image={project.image} title={project.title} link={project.link} />
+               <ProjectCard key={index} image={project.image} title={project.title} link={project.link} description={project.description} />
                ))}
               </div>
           </div>
